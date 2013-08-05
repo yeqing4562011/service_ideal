4954ef8a57bfe94f08cda978e3d88b7b609773ee
YeQing <wsmbaiwo@126.com>
2013-08-05 17:40:01 +0800

    test SSL pro

--- src/main/java/com/ideal/service/impl/FirstIdealCommonServiceImpl.java
+++ src/main/java/com/ideal/service/impl/FirstIdealCommonServiceImpl.java
@@ -8,27 +8,27 @@ import com.ideal.common.service.interf.RMIObject;
  * @Description: TODO
  * @author yq
  * @date 2013年8月4日 下午9:29:50
- * 
+ *
  */
 
 public class FirstIdealCommonServiceImpl implements IFirstIdealCommonService{
-	
+
 	public String firstHessianServicesRMI() throws Exception {
-		// TODO Auto-generated method stub 
-		System.out.println("This first rmi to service by hessian.");
+		// TODO Auto-generated method stub
+		System.out.println("This first rmi to service by hessian.....");
 		return null;
 	}
 
-	/* (非 Javadoc) 
-	* <p>Title: rmiObject</p> 
-	* <p>Description: </p> 
+	/* (非 Javadoc)
+	* <p>Title: rmiObject</p>
+	* <p>Description: </p>
 	* @param rmiObject
 	* @return
-	* @throws Exception 
-	* @see com.ideal.common.service.interf.IFirstIdealCommonService#rmiObject(com.ideal.common.service.interf.RMIObject) 
-	*/ 
-	
-	
+	* @throws Exception
+	* @see com.ideal.common.service.interf.IFirstIdealCommonService#rmiObject(com.ideal.common.service.interf.RMIObject)
+	*/
+
+
 	public RMIObject rmiObject(RMIObject rmiObject) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println(rmiObject.getName());
